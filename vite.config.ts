ac64085ee1432705206dbf3f2e8a148e7d6953e6
柳井 <108969330+Yanai1005@users.noreply.github.com>
2025-09-17 18:49:50 +0900

    viteの設定

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  base: './', // 相対パスでアセットを読み込む
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
   server: {
     host: true,
     port: 5173,
